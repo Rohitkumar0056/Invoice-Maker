322a1727f20fa76cea5a1a0e24c1d554efb518f7
anonymous-eng <harshk0056@gmail.com>
2025-04-17 23:18:17 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -30,12 +30,78 @@ app.use("/api/users", userRouter);
 app.use("/api/clients", clientRouter);
 app.use("/api/invoices", invoiceRouter);
 
+const __dirname1 = path.resolve();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: `${process.env.user}`,
+        pass: `${process.env.pass}`
+    }
+});
+
+
+var options = { format: 'A4' };
+//SEND PDF INVOICE VIA EMAIL
+app.post('/send-pdf', (req, res) => {
+    const { email } = req.body
+
+    // pdf.create(pdfTemplate(req.body), {}).toFile('invoice.pdf', (err) => {
+    pdf.create(pdfTemplate(req.body), options).toFile('invoice.pdf', (err) => {
+       
+          // send mail with defined transport object
+        transporter.sendMail({
+            from: `Invoice Maker`, // sender address
+            to: `${email}`, // list of receivers
+            replyTo: `${process.env.user}`,
+            subject: `Invoice`, // Subject line
+            text: `Invoice`, // plain text body
+            html: emailTemplate(req.body), // html body
+            attachments: [{
+                filename: 'invoice.pdf',
+                path: `${__dirname1}/invoice.pdf`
+            }]
+        });
+        if(err) {
+            res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+    });
+});
+
+//CREATE AND SEND PDF INVOICE
+app.post('/create-pdf', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('invoice.pdf', (err) => {
+        if(err) {
+            res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+    });
+});
+
+//SEND PDF INVOICE
+app.get('/fetch-pdf', (req, res) => {
+     res.sendFile(`${__dirname1}/invoice.pdf`)
+})
+
+// --------------------------deployment------------------------------
+const __dirname2 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname2, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname2, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
+
 app.use(notFound);
 app.use(errorHandler);
-
-app.get("/", (req, res) => {
-    res.send("Invoice Maker!");
-});
 
 app.listen(PORT, async () => {
     console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
